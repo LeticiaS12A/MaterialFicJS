57414f2562310d55fbc919a92cdd31ff1993e578
Letícia dos Santos Alves <leticia.s.a.tecns@gmail.com>
2025-03-12 22:01:17 -0300

    Add files via upload

--- aula08/banner-5/src/App.jsx
+++ aula08/banner-5/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="container">
       <p>Medium length banner heading with link goes here</p>
-      <img src={banner} className='close' alt="" />
+      <button className='close'>
+        <img src={banner} className='close' alt="" />
+      </button>
     </div>
   );
 }
---
+++ study-react/src/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css"
+import Example from "./class/example"
+
+export default function App(){
+    return <Example/>
+}
